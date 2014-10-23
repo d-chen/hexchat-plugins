3977f4ad7c801463ad2c9079fdcba62f36f5be44
Danny Chen <dannychen210@gmail.com>
2014-10-23 03:28:16 -0700

    add automated twitchtv bookmark creation

--- twitchChatBot.py
+++ twitchChatBot.py
@@ -11,6 +11,10 @@ import sys
 import hexchat
 import pytz
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 
 # HACK: Set default encoding to UTF-8
 if (sys.getdefaultencoding() != "utf-8"):
@@ -27,6 +31,7 @@ def local_time():
 
 FB2K_NOW_PLAYING_FILE = 'E:\Pictures\Stream\currentsong/fb2k_nowPlaying_simple.txt'
 YOUTUBE_NOW_PLAYING_FILE = 'E:\Pictures\Stream\currentsong/nowplaying_youtube_chat.txt'
+PASS_FILE = "E:\Git/xchat-plugins/twitch_pass.txt"
 COOLDOWN_PER_USER = 12
 COOLDOWN_GENERAL = 8
 BOT_LIST = ["kazukimouto", "nightbot", "brettbot", "rise_bot", "dj_jm09", "palebot"]
@@ -35,6 +40,7 @@ LOW_WIDTH_SPACE = u"\uFEFF" # insert into nicknames to avoid highlighting user e
 now_playing_source = 'FB2K'
 cooldown_time = local_time()
 last_use = {}
+time_fmt = "%H:%M %Z. %B %d, %Y"
 
 def utc_time():
     """ Return current time in UTC """
@@ -48,7 +54,6 @@ def pacific_time():
     
     pst_tz = pytz.timezone('Canada/Pacific')
     pst_dt = pst_tz.normalize(utc_dt.astimezone(pst_tz))
-    time_fmt = "%H:%M %Z. %B %d, %Y"
     hexchat.command("say Local time in Vancouver: " + pst_dt.strftime(time_fmt))
 
 def japan_time():
@@ -57,7 +62,6 @@ def japan_time():
 
     jpn_tz = pytz.timezone('Japan')
     jpn_dt = jpn_tz.normalize(utc_dt.astimezone(jpn_tz))
-    time_fmt = "%H:%M %Z. %B %d, %Y"
     hexchat.command("say Local time in Japan: " + jpn_dt.strftime(time_fmt))
 
 def flood_update(nick):
@@ -186,6 +190,54 @@ def get_channel_views(channel):
     else:
         hexchat.command("say This stream is currently offline or Twitch API is down.")
 
+def create_twitch_bookmark(channel, bookmark_name):
+    with open(PASS_FILE, 'r') as file:
+        bot_password = file.read()
+
+    title = bookmark_name.replace("!bookmark", "", 1).strip()
+    if title:
+        bookmark_title = title
+    else:
+        utc_dt = utc_time()
+        pst_tz = pytz.timezone('Canada/Pacific')
+        pst_dt = pst_tz.normalize(utc_dt.astimezone(pst_tz))
+        bookmark_title = pst_dt.strftime(time_fmt)
+
+    driver = webdriver.Firefox()
+    wait = WebDriverWait(driver, 15)
+    try:
+        driver.get("http://www.twitch.tv/{0}".format(channel[1:]))
+        # Login
+        driver.find_element_by_xpath("//span[text()=\"Log In\"]").click()
+        wait.until(lambda driver: driver.find_element_by_id("login_user_login"))
+        driver.find_element_by_id("login_user_login").clear()
+        driver.find_element_by_id("login_user_login").send_keys("low_tier_bot")
+        driver.find_element_by_id("user[password]").clear()
+        driver.find_element_by_id("user[password]").send_keys(bot_password)
+        driver.find_element_by_xpath("(//button[contains(text(), 'Log In')])").click()
+
+        # Create bookmark
+        driver.find_element_by_xpath("//span[text()=\"Bookmark\"]").click()
+        wait.until(lambda driver: driver.find_element_by_xpath("(//input[contains(@class, \"js-title\")])"))
+        driver.find_element_by_xpath("(//input[contains(@class, \"js-title\")])").clear()
+        driver.find_element_by_xpath("(//input[contains(@class, \"js-title\")])").send_keys(bookmark_title)
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        wait.until(lambda driver: driver.find_element_by_xpath("//input[contains(@value,\"twitch.tv/m/\")]"))
+        bookmark_url = driver.find_element_by_xpath("//input[contains(@value,\"twitch.tv/m/\")]").get_attribute("value")
+        hexchat.command("say Bookmark \"{0}\" created: {1} | LTB Bookmarks: {2}".format(bookmark_title, 
+                                                                                        bookmark_url,
+                                                                                        "http://www.twitch.tv/low_tier_bot/profile/bookmarks"))
+    finally:
+        driver.quit()
+        hexchat.command("say Unable to create bookmark \"{0}\".".format(bookmark_title))
+
+def is_mod(nick):
+    mod_list = hexchat.get_list("users")
+    for i in mod_list:
+        if i.nick.lower() == nick.lower() and i.prefix == "@":
+            return True
+    return False
+
 def parse(word, word_eol, userdata):
     """ Prepare messages for processing """
     str_data = word_eol[0].replace("!"," ", 1).split(None,4)    
@@ -203,13 +255,14 @@ def parse(word, word_eol, userdata):
 def route(data):
     """ Call commands if trigger present """
     command_data = data['message'].split()
+    cmd = command_data[0].lower()
     length = len(command_data)
     
-    if command_data[0] == '!ltb':
+    if cmd == '!ltb':
         if not on_cooldown(data['nick']):
-            hexchat.command("say " + data['nick'] + " -> Current active commands: !viewers !seen !jptime !wctime !sapmusic")
+            hexchat.command("say " + data['nick'] + " -> Current active commands: !bookmark !viewers !seen !jptime !wctime !sapmusic")
                 
-    if command_data[0] == '!seen':
+    if cmd == '!seen':
         if on_cooldown(data['nick']):
             return
         elif length == 2:
@@ -220,25 +273,27 @@ def route(data):
         else:
             hexchat.command("say Usage: !seen NICKNAME")
 
-    if command_data[0] == "!wctime":
+    if cmd == "!wctime":
         if not on_cooldown(data['nick']):
             pacific_time()
             
-    if command_data[0] == '!jptime':
+    if cmd == '!jptime':
         if not on_cooldown(data['nick']):
             japan_time()
 
-    if command_data[0] == '!sapmusic':
+    if cmd == '!sapmusic':
         if length == 2 and data['nick'] in ADMIN_ACCESS:
             set_now_playing_source(command_data[1])
         if not on_cooldown(data['nick']):
             now_playing()
             
-    if command_data[0] == '!viewers':
+    if cmd == '!viewers':
         if not on_cooldown(data['nick']):
             get_channel_views(data['channel'])
-            
 
+    if cmd == "!bookmark":
+        if not on_cooldown(data['nick']) and is_mod(data['nick']):
+            create_twitch_bookmark(data['channel'], data['message'])
 
 hexchat.hook_unload(db_unload)
 hexchat.hook_server('PRIVMSG', parse)
